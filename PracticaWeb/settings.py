7e9cfd91be97a1d794e77bef833f15924dba0b56
abel <abeljdrs@hotmail.com>
2019-05-28 00:07:27 +0200

    deploy

--- PracticaWeb/settings.py
+++ PracticaWeb/settings.py
@@ -33,7 +33,11 @@ ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'forkilla/static'),
